dda2e409cc51358d7a3c3774c12645edfe45d7c9
Woidt <Erik.Woidt@ordina.be>
2020-01-12 19:54:12 +0100

    move to promises

--- service/TokenService.js
+++ service/TokenService.js
@@ -3,20 +3,24 @@
 var jwt = require('jsonwebtoken');
 
 module.exports = {
-    async generateRegistrationToken(id) {
-        const token = jwt.sign({
-            exp: Math.floor(Date.now() / 1000) + Number.parseInt(process.env.TOKEN_VALID_TIME),
-            registrationId: id
-        }, process.env.SECRET_KEY)
-        return token;
+    generateRegistrationToken(id) {
+        return new Promise(function(resolve, reject){
+            const token = jwt.sign({
+                exp: Math.floor(Date.now() / 1000) + Number.parseInt(process.env.TOKEN_VALID_TIME),
+                registrationId: id
+            }, process.env.SECRET_KEY)
+            resolve(token);
+        });
     },
-    async generateLoginToken(userId) {
-        const token = jwt.sign({
-            exp: Math.floor(Date.now() / 1000) + Number.parseInt(process.env.TOKEN_VALID_TIME),
-            iat: Math.floor(Date.now() / 1000) - 300, // backdate 300 seconds
-            id: userId
-        }, process.env.SECRET_KEY)
-        return token;
+    generateLoginToken(userId) {
+        return new Promise(function(resolve, reject){
+            const token = jwt.sign({
+                exp: Math.floor(Date.now() / 1000) + Number.parseInt(process.env.TOKEN_VALID_TIME),
+                iat: Math.floor(Date.now() / 1000) - 300, // backdate 300 seconds
+                id: userId
+            }, process.env.SECRET_KEY)
+            resolve(token);
+        });
     },
     validateToken(token) {
         return new Promise(function(resolve, reject){
