67b8017dbf453c804163b93f5c3b703dde283432
Erik Woidt <erik@woidt.be>
2020-12-16 00:32:10 +0100

    update format

--- service/TshirtService.js
+++ service/TshirtService.js
@@ -12,7 +12,15 @@ exports.tshirtGET = function () {
     return new Promise(async function (resolve, reject) {
         try {
             const tshirts = await DBA.getTshirts();
-            resolve(tshirts);
+            const groups = [...new Set(tshirts.map(item => item.group.name))]
+            const t = groups.map(group_name => {
+                return {
+                    group: group_name,
+                    items: tshirts.filter(item => item.group = group_name).map(
+                        item => { return { id: item.id, name: item.name } })
+                }
+            });
+            resolve(t);
         } catch (error) {
             reject(new respondWithCode(500, {
                 code: 0,
