401f5457ddd4c943c5b5d399bc3e2a6958429182
Erik Woidt <erik@woidt.be>
2021-01-17 10:35:44 +0100

    small refactor

--- service/TshirtService.js
+++ service/TshirtService.js
@@ -20,21 +20,21 @@ exports.tshirtGET = function (language) {
                 }
                 return ((t[languageIndex]) ? t[languageIndex].description : item.group.name) //item.group.name
             }))]
-            const t = groups.map(group_name => {
+            const tgroups = groups.map(group_name => {
                 return {
                     group: group_name,
                     items: tshirts.filter(item => item.group = group_name).map(
                         sub => {
-                            let languageIndex = sub.TShirtTranslations.findIndex((x) => x.language === language);
-                            if (languageIndex == -1) {
-                                languageIndex = sub.TShirtTranslations.findIndex((x) => x.language === 'nl');
-                            }
                             const t = sub.TShirtTranslations;
+                            let languageIndex = t.findIndex((x) => x.language === language);
+                            if (languageIndex == -1) {
+                                languageIndex = t.findIndex((x) => x.language === 'nl');
+                            }
                             return { id: sub.id, name: ((t[languageIndex]) ? t[languageIndex].description : sub.name) } //sub.name
                         })
                 }
             });
-            resolve(t);
+            resolve(tgroups);
         } catch (error) {
             console.log(error)
             reject(new respondWithCode(500, {
