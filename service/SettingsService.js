2aed95c84a8db8dff627d8a6cc3eed172f1d0988
Erik Woidt <erik@woidt.be>
2021-01-25 16:55:08 +0100

    return no event

--- service/SettingsService.js
+++ service/SettingsService.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const DBA = require('../dba');
-const respondWithCode = require('../utils/writer').respondWithCode
+const respondWithCode = require('../utils/writer').respondWithCode;
 
 /**
  * get settings for frontend
@@ -9,21 +9,21 @@ const respondWithCode = require('../utils/writer').respondWithCode
  * returns Settings
  **/
 exports.settingsGET = function () {
-    return new Promise(async function (resolve, reject) {
-        const event = await DBA.getEventActive();
-        if (!event) {
-            reject(new respondWithCode(404, {
-                code: 0,
-                message: 'No Active event found'
-            }));
-        }
-        resolve({
-            startDateEvent: event.startDate.toISOString().substring(0, 10),
-            maxAge: event.maxAge,
-            minAge: event.minAge,
-            guardianAge: event.minGuardianAge,
-            tshirtDate: event.startDate.toISOString().substring(0, 10),
-            enviroment: process.env.NODE_ENV
-        });
-    })
-}
+  return new Promise(async function (resolve, reject) {
+    const event = await DBA.getEventActive();
+    if (!event) {
+      return reject(new respondWithCode(404, {
+        code: 0,
+        message: 'No Active event found'
+      }));
+    }
+    resolve({
+      startDateEvent: event.startDate.toISOString().substring(0, 10),
+      maxAge: event.maxAge,
+      minAge: event.minAge,
+      guardianAge: event.minGuardianAge,
+      tshirtDate: event.startDate.toISOString().substring(0, 10),
+      enviroment: process.env.NODE_ENV
+    });
+  });
+};
