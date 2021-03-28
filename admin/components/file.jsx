995ef6d7618316958cd19eccc2de9359bf3d5f3f
Erik Woidt <erik@woidt.be>
2021-03-28 11:47:19 +0200

    cleanup download link

--- admin/components/file.jsx
+++ admin/components/file.jsx
@@ -1,6 +1,3 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom'
-
 export const File = ({record}) => {
     return (
         <a href={record.params.downloadLink} target="_blank">download</a>
