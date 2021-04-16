2c645353e1fe2fa8bd4a0b56ee96db2720ef7008
Michael Woidt <michael.woidt@gmail.com>
2021-04-16 20:40:13 +0200

    migration

--- migrations/20210416174255-location.js
+++ migrations/20210416174255-location.js
@@ -15,7 +15,7 @@ module.exports = {
       EventId: {
         allowNull: false,
         type: Sequelize.INTEGER,
-        references: { model: 'Event', key: 'id' },
+        references: { model: 'Events', key: 'id' },
         unique: false
       },
       createdAt: {
