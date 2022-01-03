33e93bfb62d1b1954161d2a99e0148e67cc157c0
Michael Woidt <michael.woidt@gmail.com>
2022-01-03 22:23:37 +0100

    update of migration files

--- migrations/1-noname.js
+++ migrations/1-noname.js
@@ -43,377 +43,377 @@ var Sequelize = require('sequelize');
  **/
 
 var info = {
-    "revision": 1,
-    "name": "noname",
-    "created": "2021-12-01T10:24:43.067Z",
-    "comment": ""
+  'revision': 1,
+  'name': 'noname',
+  'created': '2021-12-01T10:24:43.067Z',
+  'comment': ''
 };
 
 var migrationCommands = [{
-        fn: "createTable",
-        params: [
-            "Accounts",
-            {
+  fn: 'createTable',
+  params: [
+    'Accounts',
+    {
 
-            },
-            {}
-        ]
     },
+    {}
+  ]
+},
+{
+  fn: 'createTable',
+  params: [
+    'Attachments',
     {
-        fn: "createTable",
-        params: [
-            "Attachments",
-            {
 
-            },
-            {}
-        ]
     },
+    {}
+  ]
+},
+{
+  fn: 'createTable',
+  params: [
+    'Awards',
     {
-        fn: "createTable",
-        params: [
-            "Awards",
-            {
 
-            },
-            {}
-        ]
     },
+    {}
+  ]
+},
+{
+  fn: 'createTable',
+  params: [
+    'AzureBlobs',
     {
-        fn: "createTable",
-        params: [
-            "AzureBlobs",
-            {
 
-            },
-            {}
-        ]
     },
+    {}
+  ]
+},
+{
+  fn: 'createTable',
+  params: [
+    'Certificates',
     {
-        fn: "createTable",
-        params: [
-            "Certificates",
-            {
 
-            },
-            {}
-        ]
     },
+    {}
+  ]
+},
+{
+  fn: 'createTable',
+  params: [
+    'Events',
     {
-        fn: "createTable",
-        params: [
-            "Events",
-            {
 
-            },
-            {}
-        ]
     },
+    {}
+  ]
+},
+{
+  fn: 'createTable',
+  params: [
+    'ExternVotes',
     {
-        fn: "createTable",
-        params: [
-            "ExternVotes",
-            {
 
-            },
-            {}
-        ]
     },
+    {}
+  ]
+},
+{
+  fn: 'createTable',
+  params: [
+    'Hyperlinks',
     {
-        fn: "createTable",
-        params: [
-            "Hyperlinks",
-            {
 
-            },
-            {}
-        ]
     },
+    {}
+  ]
+},
+{
+  fn: 'createTable',
+  params: [
+    'Locations',
     {
-        fn: "createTable",
-        params: [
-            "Locations",
-            {
 
-            },
-            {}
-        ]
     },
+    {}
+  ]
+},
+{
+  fn: 'createTable',
+  params: [
+    'Projects',
     {
-        fn: "createTable",
-        params: [
-            "Projects",
-            {
 
-            },
-            {}
-        ]
     },
+    {}
+  ]
+},
+{
+  fn: 'createTable',
+  params: [
+    'ProjectTables',
     {
-        fn: "createTable",
-        params: [
-            "ProjectTables",
-            {
 
-            },
-            {}
-        ]
     },
+    {}
+  ]
+},
+{
+  fn: 'createTable',
+  params: [
+    'Questions',
     {
-        fn: "createTable",
-        params: [
-            "Questions",
-            {
 
-            },
-            {}
-        ]
     },
+    {}
+  ]
+},
+{
+  fn: 'createTable',
+  params: [
+    'QuestionRegistrations',
     {
-        fn: "createTable",
-        params: [
-            "QuestionRegistrations",
-            {
 
-            },
-            {}
-        ]
     },
+    {}
+  ]
+},
+{
+  fn: 'createTable',
+  params: [
+    'QuestionTranslations',
     {
-        fn: "createTable",
-        params: [
-            "QuestionTranslations",
-            {
 
-            },
-            {}
-        ]
     },
+    {}
+  ]
+},
+{
+  fn: 'createTable',
+  params: [
+    'QuestionUsers',
     {
-        fn: "createTable",
-        params: [
-            "QuestionUsers",
-            {
 
-            },
-            {}
-        ]
     },
+    {}
+  ]
+},
+{
+  fn: 'createTable',
+  params: [
+    'Registrations',
     {
-        fn: "createTable",
-        params: [
-            "Registrations",
-            {
 
-            },
-            {}
-        ]
     },
+    {}
+  ]
+},
+{
+  fn: 'createTable',
+  params: [
+    'Sessions',
     {
-        fn: "createTable",
-        params: [
-            "Sessions",
-            {
 
-            },
-            {}
-        ]
     },
+    {}
+  ]
+},
+{
+  fn: 'createTable',
+  params: [
+    'CountTshirtSizes',
     {
-        fn: "createTable",
-        params: [
-            "CountTshirtSizes",
-            {
 
-            },
-            {}
-        ]
     },
+    {}
+  ]
+},
+{
+  fn: 'createTable',
+  params: [
+    'Tables',
     {
-        fn: "createTable",
-        params: [
-            "Tables",
-            {
 
-            },
-            {}
-        ]
     },
+    {}
+  ]
+},
+{
+  fn: 'createTable',
+  params: [
+    'TShirts',
     {
-        fn: "createTable",
-        params: [
-            "TShirts",
-            {
 
-            },
-            {}
-        ]
     },
+    {}
+  ]
+},
+{
+  fn: 'createTable',
+  params: [
+    'TShirtGroups',
     {
-        fn: "createTable",
-        params: [
-            "TShirtGroups",
-            {
 
-            },
-            {}
-        ]
     },
+    {}
+  ]
+},
+{
+  fn: 'createTable',
+  params: [
+    'TShirtGroupTranslations',
     {
-        fn: "createTable",
-        params: [
-            "TShirtGroupTranslations",
-            {
 
-            },
-            {}
-        ]
     },
+    {}
+  ]
+},
+{
+  fn: 'createTable',
+  params: [
+    'TShirtTranslations',
     {
-        fn: "createTable",
-        params: [
-            "TShirtTranslations",
-            {
 
-            },
-            {}
-        ]
     },
+    {}
+  ]
+},
+{
+  fn: 'createTable',
+  params: [
+    'Users',
     {
-        fn: "createTable",
-        params: [
-            "Users",
-            {
 
-            },
-            {}
-        ]
     },
+    {}
+  ]
+},
+{
+  fn: 'createTable',
+  params: [
+    'ShowUserWithNoProject',
     {
-        fn: "createTable",
-        params: [
-            "ShowUserWithNoProject",
-            {
 
-            },
-            {}
-        ]
     },
+    {}
+  ]
+},
+{
+  fn: 'createTable',
+  params: [
+    'userprojectvideo',
     {
-        fn: "createTable",
-        params: [
-            "userprojectvideo",
-            {
 
-            },
-            {}
-        ]
     },
+    {}
+  ]
+},
+{
+  fn: 'createTable',
+  params: [
+    'ShowAttachmentLoaded',
     {
-        fn: "createTable",
-        params: [
-            "ShowAttachmentLoaded",
-            {
 
-            },
-            {}
-        ]
     },
+    {}
+  ]
+},
+{
+  fn: 'createTable',
+  params: [
+    'Votes',
     {
-        fn: "createTable",
-        params: [
-            "Votes",
-            {
 
-            },
-            {}
-        ]
     },
+    {}
+  ]
+},
+{
+  fn: 'createTable',
+  params: [
+    'VoteCategories',
     {
-        fn: "createTable",
-        params: [
-            "VoteCategories",
-            {
 
-            },
-            {}
-        ]
     },
+    {}
+  ]
+},
+{
+  fn: 'createTable',
+  params: [
+    'Vouchers',
     {
-        fn: "createTable",
-        params: [
-            "Vouchers",
-            {
 
-            },
-            {}
-        ]
     },
+    {}
+  ]
+},
+{
+  fn: 'addIndex',
+  params: [
+    'AzureBlobs',
+    ['container_name', 'blob_name'],
     {
-        fn: "addIndex",
-        params: [
-            "AzureBlobs",
-            ["container_name", "blob_name"],
-            {
-                "indexName": "azure_blobs_container_name_blob_name",
-                "indicesType": "UNIQUE"
-            }
-        ]
-    },
-    {
-        fn: "addIndex",
-        params: [
-            "QuestionTranslations",
-            ["questionId", "language"],
-            {
-                "indexName": "question_translations_question_id_language",
-                "indicesType": "UNIQUE"
-            }
-        ]
-    },
-    {
-        fn: "addIndex",
-        params: [
-            "TShirtGroupTranslations",
-            ["tShirtGroupId", "language"],
-            {
-                "indexName": "t_shirt_group_translations_t_shirt_group_id_language",
-                "indicesType": "UNIQUE"
-            }
-        ]
-    },
-    {
-        fn: "addIndex",
-        params: [
-            "TShirtTranslations",
-            ["tShirtId", "language"],
-            {
-                "indexName": "t_shirt_translations_t_shirt_id_language",
-                "indicesType": "UNIQUE"
-            }
-        ]
+      'indexName': 'azure_blobs_container_name_blob_name',
+      'indicesType': 'UNIQUE'
     }
+  ]
+},
+{
+  fn: 'addIndex',
+  params: [
+    'QuestionTranslations',
+    ['questionId', 'language'],
+    {
+      'indexName': 'question_translations_question_id_language',
+      'indicesType': 'UNIQUE'
+    }
+  ]
+},
+{
+  fn: 'addIndex',
+  params: [
+    'TShirtGroupTranslations',
+    ['tShirtGroupId', 'language'],
+    {
+      'indexName': 't_shirt_group_translations_t_shirt_group_id_language',
+      'indicesType': 'UNIQUE'
+    }
+  ]
+},
+{
+  fn: 'addIndex',
+  params: [
+    'TShirtTranslations',
+    ['tShirtId', 'language'],
+    {
+      'indexName': 't_shirt_translations_t_shirt_id_language',
+      'indicesType': 'UNIQUE'
+    }
+  ]
+}
 ];
 
 module.exports = {
-    pos: 0,
-    up: function(queryInterface, Sequelize)
-    {
-        var index = this.pos;
-        return new Promise(function(resolve, reject) {
-            function next() {
-                if (index < migrationCommands.length)
-                {
-                    let command = migrationCommands[index];
-                    console.log("[#"+index+"] execute: " + command.fn);
-                    index++;
-                    queryInterface[command.fn].apply(queryInterface, command.params).then(next, reject);
-                }
-                else
-                    resolve();
-            }
-            next();
-        });
-    },
-    info: info
+  pos: 0,
+  up: function(queryInterface, Sequelize)
+  {
+    var index = this.pos;
+    return new Promise(function(resolve, reject) {
+      function next() {
+        if (index < migrationCommands.length)
+        {
+          let command = migrationCommands[index];
+          console.log('[#'+index+'] execute: ' + command.fn);
+          index++;
+          queryInterface[command.fn].apply(queryInterface, command.params).then(next, reject);
+        }
+        else
+          resolve();
+      }
+      next();
+    });
+  },
+  info: info
 };
