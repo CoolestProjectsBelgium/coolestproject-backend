de6081d6f6f870e350548bd16a8fdef0929d437e
Michael Woidt <michael.woidt@gmail.com>
2022-01-16 17:58:29 +0100

    seeder split of hiestorical and new data

--- seeders/20201115141500-events.js
+++ seeders/20201115141500-events.js
@@ -18,22 +18,6 @@ module.exports = {
         maxRegistration: 48, 
         maxVoucher: 3, 
         event_title: 'Coolest Projects 2021'
-      },
-      { 
-        eventBeginDate: new Date('2021-11-01'), 
-        registrationOpenDate: new Date('2022-02-01'), 
-        registrationClosedDate: new Date('2022-03-31'),
-        projectClosedDate: new Date('2022-04-09'),
-        officialStartDate: new Date('2022-04-24'),
-        eventEndDate: new Date('2022-08-25'),
-        createdAt: new Date(), 
-        updatedAt: new Date(), 
-        minAge: 7, 
-        maxAge: 18, 
-        minGuardianAge: 16, 
-        maxRegistration: 48, 
-        maxVoucher: 3, 
-        event_title: 'Coolest Projects 2022'
       }
     ], {});
   },
--- seeders_dataLoad/20201115141500-events.js
+++ seeders_dataLoad/20201115141500-events.js
@@ -1,24 +1,9 @@
 'use strict';
-
+const tname = 'Events';
+var back;
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('Events', [
-      { 
-        eventBeginDate: new Date('2021-04-25'), 
-        registrationOpenDate: new Date('2021-05-25'), 
-        registrationClosedDate: new Date('2021-06-25'),
-        projectClosedDate: new Date('2021-07-25'),
-        officialStartDate: new Date('2021-08-25'),
-        eventEndDate: new Date('2021-09-25'),
-        createdAt: new Date(), 
-        updatedAt: new Date(), 
-        minAge: 7, 
-        maxAge: 18, 
-        minGuardianAge: 16, 
-        maxRegistration: 48, 
-        maxVoucher: 3, 
-        event_title: 'Coolest Projects 2021'
-      },
       { 
         eventBeginDate: new Date('2021-11-01'), 
         registrationOpenDate: new Date('2022-02-01'), 
@@ -38,6 +23,6 @@ module.exports = {
     ], {});
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkDelete('Events', null, {});
+    await queryInterface.sequelize.query( `DELETE FROM '${tname}'  WHERE EventId = 2;`,back);
   }
 };
