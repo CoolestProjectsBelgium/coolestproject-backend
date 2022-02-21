2e3b347ddc79e40dd52e7c2c169a7ddd5b58b521
Michael Woidt <michael.woidt@gmail.com>
2022-02-21 21:59:44 +0100

    Presentation handlebars added

--- website/index.js
+++ website/index.js
@@ -157,6 +157,7 @@ router.get('/planning/:eventId/', cors(corsOptions), async function (req, res, n
         if (endTime == '00:00'){ endTime = 0; yesdescript = false; Needs = true};
         if (testTime == '00:00'){ endTime = 0; yesdescript = false; Needs = true};
         let requirements = project.get('project_type');
+    
 
         projectList.push({
           'style': cardStyle,
@@ -176,7 +177,7 @@ router.get('/planning/:eventId/', cors(corsOptions), async function (req, res, n
           'yesdescript': yesdescript
         }) 
         //console.log(projectList)  ;
-        
+
         const url = req.originalUrl  ;
         const last = url.charAt(url.length - 1);
         if (last !='/'){eventId = last}
@@ -190,9 +191,123 @@ router.get('/planning/:eventId/', cors(corsOptions), async function (req, res, n
       }
     }
   }
- //console.log(result)  
+ //console.log(result)
+
 
   res.render('calendar.handlebars', { 
+    eventName: event.event_title, 
+    eventDate: new Intl.DateTimeFormat('nl-BE', {  dateStyle: 'short' }).format(event.officialStartDate), 
+    grid: result 
+    })
+}),
+
+router.get('/presentation/:eventId/', cors(corsOptions), async function (req, res, next) {
+  // create a planning table structure 
+  // x -> list of locations
+  // y -> list of table
+  // z -> list of projects
+
+  //console.log("=====parameter input:...planning/2/?Project=##")
+  //console.log(req.query)
+  
+
+  const event = await Event.findByPk(req.params.eventId)
+  if (event === null) {
+    return next(new Error('event not found'))
+  }
+  const locations = await event.getLocations() 
+  const tablesGroupedCount = await Table.findAll(
+    {
+      attributes: ['LocationId','EventId', [Sequelize.fn('count', Sequelize.col('LocationId')), 'count']],
+      group: ['LocationId','EventId'],
+      having : { 'EventId': event.get('id') }
+    }
+  )
+  // we need header values in the first row so + 1 for length
+  const maxTablesCount = Math.max(...tablesGroupedCount.map(o => o.get('count')), 0) + 1;
+
+  const result = Array(maxTablesCount).fill().map(() => Array(locations.length));
+  for (const [i, location] of locations.entries()) {
+    result[0][i] = {
+      name: location.get('text'),
+      header: true 
+    }
+
+    let tables = await location.getTables()
+
+    for (const [j, table] of tables.entries()) {
+      let projectList = []
+      let projects = await table.getProjects()
+      let tableEquip = "";
+
+      if(projects!=[]){
+          for(let project of projects){
+
+            if(!req.query.ProjectId || project.get('id') == req.query.ProjectId){
+              let participantsList = []
+              let owner = await project.getOwner()
+              let agreedToPhoto = true
+
+              if(owner){
+                participantsList.push(owner)
+                agreedToPhoto = agreedToPhoto && (await owner.getQuestions()).some((ele) => { return ele.name == 'Agreed to Photo' })
+              }
+              let participants = await project.getParticipant()
+          
+              if(participants){
+                for(let participant of participants){
+                  agreedToPhoto = agreedToPhoto && (await participant.getQuestions()).some((ele) => { return ele.name == 'Agreed to Photo' })
+                }
+                participantsList.push(...participants) 
+              } 
+
+              let attachments = await project.getAttachments({ where: { confirmed: true } })
+        
+              let cardStyle = ''
+              if(!agreedToPhoto){
+                cardStyle = 'border-danger'
+                } else if(project.get('project_lang') == 'nl'){
+                  cardStyle = 'border-primary'
+                } else if(project.get('project_lang') == 'fr'){
+                  cardStyle = 'border-secondary'
+                } 
+              let endTime = new Intl.DateTimeFormat('nl-BE', {  timeStyle: 'short' }).format(project.ProjectTable.get('endTime'));
+              let startTime = new Intl.DateTimeFormat('nl-BE', { dateStyle: 'medium', timeStyle: 'short' }).format(project.ProjectTable.get('startTime'));
+              let testTime = new Intl.DateTimeFormat('nl-BE', {  timeStyle: 'short' }).format(project.ProjectTable.get('startTime'));   
+
+              projectList.push({
+                'style': cardStyle,
+                'language': project.get('project_lang'),
+                'name': table.name,
+                'projectName': project.get('project_name'),
+                'projectID': project.get('id'),
+                'participants': participantsList.map((ele) => { return ele.get('firstname') + ' ' + ele.get('lastname') } ).join(', '),
+                'link': (await attachments.pop()?.getHyperlink())?.get('href'),
+                'description': project.get('project_descr'),
+                'agreedToPhoto': agreedToPhoto
+              }) 
+              console.log("====>projectList")  
+              console.log(projectList)  
+            
+              result[j+1][i] = {
+                name: table.get('name'),
+                projects: projectList
+                }
+            
+          
+            }
+        
+          }
+        }
+     }
+      
+  }
+
+ 
+  console.log(result)  
+
+
+  res.render('presentation.handlebars', { 
     eventName: event.event_title, 
     eventDate: new Intl.DateTimeFormat('nl-BE', {  dateStyle: 'short' }).format(event.officialStartDate), 
     grid: result 
