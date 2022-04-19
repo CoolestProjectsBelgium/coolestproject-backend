9ccbb53f4a10ad33c8385681a0d1efb5674e4f90
Michael Woidt <michael.woidt@gmail.com>
2022-04-19 19:14:16 +0200

    rapi proc changed

--- static/raspiScripts/01_run-on-raspi.sh
+++ static/raspiScripts/01_run-on-raspi.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-scp LoopGetProjects.sh pi@192.168.2.209:/home/pi
+scp LoopGetProjectsOnRaspi.sh pi@192.168.2.209:/home/pi
 sleep 3
-sshpass -vvv -p coolestprojects ssh pi@192.168.2.209 bash LoopGetProjects.sh
+sshpass -vvv -p coolestprojects ssh pi@192.168.2.209 bash LoopGetProjectsOnRaspi.sh
--- static/raspiScripts/LoopGetProjects.sh
+++ static/raspiScripts/LoopGetProjects.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-
-base_url="https://backend.coolestprojects.localhost:1234/website/"
+#base_url="https://backend.coolestprojects.be/website/"
+base_url="https://backend-test.coolestprojects.be/website/"
+#base_url="https://backend.coolestprojects.localhost:1234/website/"
 wait_time=10
 old_process=-1
-
 while :
 do
 	# get latest project ids
@@ -11,33 +11,26 @@ do
 	list=$( echo $data | jq -c '.[].id' )
 	
 	# display special pages
-	/usr/bin/google-chrome https://coolestprojects.be --incognito --kiosk  & disown
+	DISPLAY=:0.0 XAUTHORITY=/home/pi/.Xauthority chromium-browser https://coolestprojects.be/ --incognito  --kiosk  & disown
 	process=$!
-	echo $process
 	sleep $wait_time
-	kill $process
+	echo new: $process old: $old_process
+	#kill $process
+	old_process=$process
 	
 	# loop projects
 	for project_id in $list
 	do
-		/usr/bin/google-chrome ${base_url}video-presentation/2/?ProjectId=$project_id --incognito --kiosk --autoplay-policy=no-user-gesture-required & disown
+	echo ProjectId: $project_id
+		DISPLAY=:0.0 XAUTHORITY=/home/pi/.Xauthority chromium-browser ${base_url}video-presentation/2/?ProjectId=$project_id --disable-gpu --incognito --kiosk & disown
 		process=$!
-		echo $process
 		sleep $wait_time
-		
 		if [ $old_process -gt 0 ] 
 		then
 			kill $old_process
 		fi
-		
-		old_process = process
+		echo new: $process old: $old_process
+		old_process=$process
 	done
-	
-	# display special pages
-	/usr/bin/google-chrome https://coolestprojects.be/fr --incognito --kiosk  & disown
-	process=$!
-	echo $process
-	sleep $wait_time
-	kill $process
-	
+	kill $old_process
 done
