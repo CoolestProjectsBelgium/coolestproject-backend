5e5f8e8e10ab0de5d5fe8d76c51ff8771b448f45
Tom Haepers <tom.haepers@outlook.be>
2023-01-05 00:48:45 +0100

    Fix SSL

--- models/index.js
+++ models/index.js
@@ -8,11 +8,24 @@ const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
+// This cannot be done in config.json
+// Do we want to make this configurable?
+const ssl = {
+  dialectOptions: {
+    ssl  : {
+      ca : fs.readFileSync(__dirname + '/DigiCertGlobalRootCA.crt.pem')
+      }  
+    }
+  };
+
+const configssl = Object.assign(config, ssl); // Merge config.json and the SSL addition
+console.log('Sequelize DB Options: %s', configssl); // Test
+
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  sequelize = new Sequelize(process.env[config.use_env_variable], configssl);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(config.database, config.username, config.password, configssl);
 }
 
 fs
