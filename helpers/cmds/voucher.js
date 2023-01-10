6695d87be1eea89eb105d4c5603eebfc67af4267
Michael Woidt <michael.woidt@gmail.com>
2023-01-10 13:25:47 +0100

    linter changes

--- helpers/cmds/voucher.js
+++ helpers/cmds/voucher.js
@@ -1,22 +1,22 @@
 const DBA = require('../../dba');
 
-exports.command = `voucher`
-exports.desc = `Voucher related commands`
+exports.command = 'voucher';
+exports.desc = 'Voucher related commands';
 
 exports.builder = (yargs) => {
-    yargs.command('create <userId>', 'Create voucher for project',
-        () => { },
-        async (argv) => {
-            try {
-                const voucher = await DBA.createVoucher(argv.userId);
-                console.log(`Voucher created for user  ${argv.userId} token ${voucher.id}`);
+  yargs.command('create <userId>', 'Create voucher for project',
+    () => { },
+    async (argv) => {
+      try {
+        const voucher = await DBA.createVoucher(argv.userId);
+        console.log(`Voucher created for user  ${argv.userId} token ${voucher.id}`);
 
-            } catch (error) {
-                console.log(error.message)
-                for (var err of error.errors || []) {
-                    console.error(err.message)
-                }
-            }
+      } catch (error) {
+        console.log(error.message);
+        for (var err of error.errors || []) {
+          console.error(err.message);
         }
-    )
+      }
+    }
+  );
 };
