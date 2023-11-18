0fe3a6c9388fc6c80c559e82f5c2970ac0be8ef8
Erik Woidt <erik@woidt.be>
2023-11-18 19:52:15 +0100

    add runtime env

---
+++ ecosystem.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+    apps: [
+      {
+        name: 'coderdojo-backend',
+        script: './dist/app.js'
+      },
+    ],
+  };
--- src/app.ts
+++ src/app.ts
@@ -11,6 +11,7 @@ AdminJS.registerAdapter({
 })
 
 const PORT = 3000
+const HOST = '0.0.0.0'
 
 async function start() {
   const app = express()
@@ -23,7 +24,7 @@ async function start() {
   const adminRouter = AdminJSExpress.buildRouter(admin)
   app.use(admin.options.rootPath, adminRouter)
 
-  app.listen(PORT, () => {
+  app.listen(PORT, HOST, () => {
     console.log(`AdminJS started on http://localhost:${PORT}${admin.options.rootPath}`)
   })
 }
