34c6bfa08747e89592ecae0b88963c30f84aae73
Tom Haepers <tommy.haepers@gmail.com>
2026-03-15 16:40:42 +0100

    Updated view showing table

--- queries/Missing uploads.sql
+++ queries/Missing uploads.sql
@@ -1,19 +1,29 @@
 CREATE OR REPLACE VIEW `missing-uploads` AS
-SELECT 
-	p.eventid AS EventId,
-    p.id AS ProjectId,        
-	p.project_name AS project_name,
-	u.firstname AS firstname,
-	u.lastname AS lastname,
-	p.ownerId AS ownerId,
-	u.email,
-	u.email_guardian,
-	u.language
-FROM
-	projects p        
-	inner JOIN users u on u.id = p.ownerId
-	LEFT OUTER JOIN attachments a on p.id = a.ProjectId
-WHERE
-	p.eventid = (SELECT id FROM events WHERE registrationClosedDate <= CURDATE() and CURDATE() <= eventEndDate)
-	AND a.name is NULL
-ORDER BY u.lastname
+    SELECT 
+        p.eventId AS EventId,
+        p.id AS ProjectId,
+        p.project_name AS project_name,
+        t.name AS TableName,
+        u.firstname AS firstname,
+        u.lastname AS lastname,
+        p.ownerId AS ownerId,
+        u.email AS email,
+        u.email_guardian AS email_guardian,
+        u.language AS language
+    FROM
+        projects p
+        JOIN users u ON u.id = p.ownerId
+        LEFT JOIN attachments a ON p.id = a.ProjectId
+        LEFT JOIN projecttables pt ON pt.ProjectId = p.id
+        LEFT JOIN `tables` t ON pt.TableId = t.id
+    WHERE
+        p.eventId = (SELECT 
+						events.id
+					FROM
+						events
+					WHERE
+						events.registrationClosedDate <= CURDATE() 
+                        AND CURDATE() <= events.eventEndDate
+                )
+            AND a.name IS NULL
+    ORDER BY u.lastname;
